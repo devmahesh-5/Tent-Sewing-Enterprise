c9ef2ba331541fb31aa45f5e3f5cf7a704d12394
Mahesh Bhandari <n31mahesh@gmail.com>
2025-01-01 18:40:57 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>This is Start of making a tent sewing's official Website</h1>
+    </>
+  )
+}
+
+export default App
