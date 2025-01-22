2bfb31b5abce09d73d22aba9a8721cf545b7eb86
Mahesh Bhandari <n31mahesh@gmail.com>
2025-01-22 22:38:34 +0545

    backend and frontend connection

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,15 +1,28 @@
+import axios from "axios";
 export class AuthService {
 
     async registerUser({ name, email, password, role }) {
         try {
+            const response = await axios.post('/api/v1/users/register', { name, email, password, role });
 
+            if (!response) {
+                throw new Error("User Registration Failed");
+            }
+
+            return response.data;
         } catch (error) {
             console.log("User Registration Error", error);
         }
     }
     async loginUser({ email, password }) {
         try {
+            const response = await axios.post('/api/v1/users/login', { email, password });
 
+            if (!response) {
+                throw new Error("User Login Failed");
+            }
+
+            return response.data;
         } catch (error) {
             console.log("User Login Error", error);
         }
@@ -17,15 +30,42 @@ export class AuthService {
 
     async logoutUser() {
         try {
+            const response = await axios.post('/api/v1/users/logout');
+
+            if (!response) {
+                throw new Error("User Logout Failed");
+            }
+
+            return response.data;
 
         } catch (error) {
             console.log("User Logout Error", error);
         }
     }
 
+    async updatePassword({ oldPassword, newPassword }) {
+        try {
+            const response = await axios.post('/api/v1/users/update-password', { oldPassword, newPassword });            
+
+            if (!response) {
+                throw new Error("Password Update Failed");
+            }
+
+            return response.data;
+        } catch (error) {
+                console.log("Password Update Error", error);
+            }
+    }
+
     async getCurrentUser() {
         try {
+            const response = await axios.get('/api/v1/users/myprofile');
 
+            if (!response) {
+                throw new Error("Get User Failed");
+            }
+
+            return response.data;
         } catch (error) {
             console.log("Get User Error", error);
         }
--- src/services/config.js
+++ src/services/config.js
@@ -1,24 +1,109 @@
 export class Services {
-    async addProducts(){
+    async addProducts({title, description, price, category, status, image}){
+        try {
+            const response = await axios.post('/api/v1/products/create',{title, description, price, category, status, image});
+    
+            if (!response) {
+                throw new Error("Product Creation Failed");
+            }
+    
+            return response.data;
+        } catch (error) {
+            throw new Error("Product Creation Failed");
+        }
 
     }
-
-    async addAchivements(){
-
-    }
-
     async getAllProducts(){
+        try {
+            const response = await axios.get('/api/v1/products/all-products');
+    
+            if (!response) {
+                throw new Error("Product Creation Failed");
+            }
+    
+            return response.data;
+        } catch (error) {
+            console.log("Product Creation Error", error);
+            throw new Error("Product Creation Failed");
+        }
+    }
+    async getProductByCategory(category){
+        try {
+            const response = await axios.get(`/api/v1/products/category/${category}`);
+    
+    if(!response){
+        throw new Error("Product fetching Failed");
         
+                }
+    
+                return response.data
+        } catch (error) {
+            console.log("Product Creation Error", error);
+            
+        }
     }
-    async getProductByCategory(){
 
+    async getProductById(id){
+        try {
+            const response = await axios.get(`/api/v1/products/${id}`);
+    
+            if(!response){
+                throw new Error("Product fetching Failed");
+                }
+    
+                return response.data
+        } catch (error) {
+            console.log("Product fetching Error", error);
+            throw new Error("something went wrong while fetching product");
+            
+        }
     }
 
-    async getAchivement(){
-
+    async deleteProduct(id){
+        try {
+            const response = await axios.delete(`/api/v1/products/${id}`);
+    
+            if(!response){
+                throw new Error("Product deletion Failed");
+                }
+    
+                return response.data
+        } catch (error) {
+            console.log("Product deletion Error", error);
+            throw new Error("something went wrong while deleting product");
+        }
     }
+
+    async updateProduct(id, {title, description, price, category, status, image}){
+        try {
+            const response = await axios.post(`/api/v1/products/${id}`, {title, description, price, category, status, image});
+    
+            if(!response){
+                throw new Error("Product Updation Failed");
+            }
+    
+            return response.data
+        } catch (error) {
+            console.log("Product Updation Error", error);
+            throw new Error("something went wrong while updating product");
+        }
+    }
+    async addAchivements(){
+        try {
+            
+        } catch (error) {
+            console.log("achivement Creation Error", error);
+            throw new Error("something went wrong while creating ac");
+        }
+    }
+
     async getAllAchivements(){
-
+        try {
+            
+        } catch (error) {
+            console.log("achivement fetching Error", error);
+            throw new Error("something went wrong while fetching achivement");
+        }
     }
 }
 
